a47f01efd45e5ea1131311e970f2d111a3d874ad
thisisnotme <botepobel@gmail.com>
2012-04-20 16:04:44 +0200

    Licencing

--- fr/legbt/sections/Sections.java
+++ fr/legbt/sections/Sections.java
@@ -1,3 +1,22 @@
+/* 
+ *    This file is part of Sections.
+ *    Copyright © 2012 Alban Avenant
+ *
+ *     Sections is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *     
+ *     Sections is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *     
+ *     You should have received a copy of the GNU General Public License
+ *     along with Sections.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package fr.legbt.sections;
 
 import java.awt.Frame;
